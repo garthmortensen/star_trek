e33d3ecebe6b262a001d3dc0dfa42a5c95c38976
Garth <mortensengarth@hotmail.com>
2021-01-31 18:08:11 -0600

    a bit of cleansing

--- starting.py
+++ starting.py
@@ -12,6 +12,12 @@ This is a temporary script file.
 # =============================================================================
 
 import os  # for folders
+import logging  # to replace print()
+import re  # regex for filtering text
+
+# automate the boring stuff 1 ed pg 221
+logging.basicConfig(level=logging.DEBUG, format=" %(asctime)s - %(levelname)s - %(message)s")
+logging.debug("program start")
 
 # =============================================================================
 # declare and set variables
@@ -41,11 +47,111 @@ for each_file in files_in[0:N_SCRIPTS]:  # first 2 as a sample
 
     scripts.append(lines)
 
-del dir_script, each_file, f, files_in, lines
+del dir_script, each_file, f, files_in, lines, dir_in
+
+# %%
 
 # =============================================================================
-# clean the scripts
+# remove blank lines and \n from script, and lowercase
 # =============================================================================
+
+no_blanks_list = []
+
+for each_script in scripts:
+    
+    each_script_no_blanks = []
+    
+    for each_line in each_script:
+        if each_line.isspace() == False:
+            
+            logging.debug(each_line.strip().lower())
+            each_script_no_blanks.append(each_line.strip().lower())  # remove \n
+    
+    no_blanks_list.append(each_script_no_blanks)
+    
+del scripts, each_script, each_script_no_blanks, each_line
+
+# %%
+
+# =============================================================================
+# keep only alphanumeric characters
+# =============================================================================
+
+alphanumeric_regex = re.compile(r"[a-zA-Z0-9]")
+
+only_alphanumeric_list = []
+
+for each_script in no_blanks_list:
+    
+    each_script_only_only_alphanumeric_list = []
+    
+    for each_line in each_script:
+        
+        alphanumeric_regex.findall(each_line)
+        
+        # logging.debug(each_line.strip())
+        each_script_only_only_alphanumeric_list.append(each_line.strip())  # remove \n
+    
+    only_alphanumeric_list.append(each_script_only_only_alphanumeric_list)
+    
+del no_blanks_list, each_script, each_script_only_only_alphanumeric_list, each_line
+
+
+# %%
+
+alpha_only_list = []
+
+for each_script in scripts:
+
+    for each_line in each_script:
+    
+        # lower the article
+        each_line = each_line.lower()
+        
+        # split by white spaces
+        words = each_line.split()
+    
+        # remove all tokens that are not alphabetic
+        words = [word for word in words if word.isalpha()]
+    
+        # remove words < char len 2 and > 15
+        words = [word for word in words if len(word) > 2 and len(word) < 15]
+        
+        alpha_only_list.append(words)
+
+
+# %%
+
+
+sample = no_blanks_list[0][6]
+
+
+alphanumeric_regex = re.compile(r"[a-zA-Z0-9 ]")  #alphanumeric and space
+
+out = alphanumeric_regex.findall(sample)
+
+
+
+# %%
+
+alphanumeric_regex = re.compile(r"[a-zA-Z0-9]")
+
+only_alphanumeric_list = []
+
+for each_script in no_blanks_list:
+    
+    each_script_only_only_alphanumeric_list = []
+    
+    for each_line in each_script:
+        
+        alphanumeric_regex.findall(each_line)
+        
+        # logging.debug(each_line.strip())
+        each_script_only_only_alphanumeric_list.append(each_line.strip())  # remove \n
+    
+    only_alphanumeric_list.append(each_script_only_only_alphanumeric_list)
+    
+del no_blanks_list, each_script, each_script_only_only_alphanumeric_list, each_line
 
 
 
